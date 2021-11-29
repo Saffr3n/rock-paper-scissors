c3a18a57aa29711d7d3fbed993d5d17602613f7b
Saffr3n <saffr3n@gmail.com>
2021-11-30 01:52:21 +0200

    Added UI and refactored script.js for it, updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rock Paper Scissors
 
-Simple implementation of the game for browser console
+Simple implementation of the game. First player to score 5 points wins the game. Try your luck against computer!
--- script.js
+++ script.js
@@ -1,106 +1,49 @@
-// Array of valid options
 const RPS = ['Rock', 'Paper', 'Scissors'];
+const buttons = document.querySelectorAll('button');
+const  scoreDisplay = document.querySelector('h2');
+const  roundCounterDisplay = document.querySelector('h3');
+const  roundResultDisplay = document.querySelector('div');
 
-// Number of game rounds
-const ROUNDS = 5;
+let roundCounter = 1;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+let endGameMessage;
 
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+    roundCounter++;
+    roundCounterDisplay.textContent = `Round ${roundCounter}`;
+    roundResultDisplay.textContent = playRound(i, computerPlay());
 
-// Computer makes a random choice
-function computerPlay() {
-  return Math.floor(Math.random() * RPS.length);
-}
+    if (roundResultDisplay.textContent.includes('win')) playerWinCounter++;
+    if (roundResultDisplay.textContent.includes('lose')) computerWinCounter++;
 
-// Player makes a choice
-function playerPlay() {
-  
-  // Prompt player for input
-  let playerInput;
-  while(true) {
-    playerInput = prompt('Play your turn: ');
-    if (playerInput) break;
-  }
-  
-  // Canonicalize the input
-  playerInput = playerInput.toLowerCase().replace(playerInput[0], playerInput[0].toUpperCase());
-  
-  // Return the input value if it's valid
-  for (let i = 0; i < RPS.length; i++) {
-    if (playerInput === RPS[i]) {
-      return i;
+    scoreDisplay.textContent = `Player ${playerWinCounter} : ${computerWinCounter} Computer`;
+
+    if (playerWinCounter === 5) endGameMessage = 'You win! Congratulations!';
+    if (computerWinCounter === 5) endGameMessage = 'You lose... Better luck next time.';
+
+    if (endGameMessage) {
+      alert(endGameMessage);
+      window.location.reload();
     }
-  }
-  
-  // Otherwise return the function (i.e. restart it)
-  return playerPlay();
+  });
 }
 
-// Play a game round
 function playRound(playerSelection, computerSelection) {
-
-  // Win condition
   if (playerSelection === computerSelection + 1 || playerSelection === computerSelection - (RPS.length - 1)) {
-  
-    // Use "beat" if won with scissors, otherwise "beats"
     let s = playerSelection === 2 ? 'beat' : 'beats';
-    
-    // Return win message
     return `You win! ${RPS[playerSelection]} ${s} ${RPS[computerSelection]}`;
   }
-  
-  // Lose condition
+
   if (playerSelection === computerSelection - 1 || playerSelection === computerSelection + (RPS.length - 1)) {
-  
-    // Use "beat" if lost to scissors, otherwise "beats"
     let s = computerSelection === 2 ? 'beat' : 'beats';
-    
-    // Return lose message
     return `You lose! ${RPS[computerSelection]} ${s} ${RPS[playerSelection]}`;
   }
-  
-  // Return tie message if none of conditions above were met
+
   return 'Tie!';
 }
 
-// Play a game
-function game() {
-
-  // Declare win counters
-  let playerWinCount = 0;
-  let computerWinCount = 0;
-  
-  // Play amount of ROUNDS
-  for (let i = 0; i < ROUNDS; i++) {
-    let s = playRound(playerPlay(), computerPlay());
-    
-    // Update counters
-    if (s.includes('win')) playerWinCount++;
-    if (s.includes('lose')) computerWinCount++;
-    
-    // Show result of the round
-    console.log(s);
-  }
-  
-  console.log('\nResult:');
-  
-  // Show win message if player score is higher
-  if (playerWinCount > computerWinCount) {
-    console.log(
-      `Congratulations! You won with the score of ${playerWinCount} against computer score of ${computerWinCount}`
-    );
-  }
-  
-  // Show lose message if computer score is higher
-  else if (playerWinCount < computerWinCount) {
-    console.log(
-      `Unlucky... You lost with the score of ${playerWinCount} against computer score of ${computerWinCount}`
-    );
-  }
-  
-  // Show tie message if scores are equal
-  else {
-    console.log(`Tie game. Your score of ${playerWinCount} equals computer score`);
-  }
+function computerPlay() {
+  return Math.floor(Math.random() * RPS.length);
 }
-
-// Start the game
-game();
